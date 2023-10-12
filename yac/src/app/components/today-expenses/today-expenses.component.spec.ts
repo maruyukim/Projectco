526edc2e24887fb7f0039d218a8f8513c60d4824
Mengya Shi <shi00077@algonquinlive.com>
2023-10-11 21:52:29 -0400

    first push test

---
+++ yac/src/app/components/today-expenses/today-expenses.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+
+import { TodayExpensesComponent } from './today-expenses.component';
+
+describe('TodayExpensesComponent', () => {
+  let component: TodayExpensesComponent;
+  let fixture: ComponentFixture<TodayExpensesComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [ TodayExpensesComponent ],
+      imports: [IonicModule.forRoot()]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(TodayExpensesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ yac/src/app/components/today-expenses/today-expenses.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-today-expenses',
+  templateUrl: './today-expenses.component.html',
+  styleUrls: ['./today-expenses.component.scss'],
+})
+export class TodayExpensesComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
